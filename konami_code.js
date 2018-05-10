c10faa5999882b2a42da326a338047b61bca8ea3
Jennifer Dobak <jdobak1@gmail.com>
2018-05-10 17:29:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,14 +2,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init() {
+function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
 
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
 
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
 
       index = 0;
